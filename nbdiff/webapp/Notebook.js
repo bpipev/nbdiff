91fec4f7407f6fc3895b79abd731af9ce1f7c0f4
bsplay <bsplay@github.com>
2014-01-18 17:39:08 -0500

    first commit of webapp

---
+++ nbdiff/webapp/Notebook.js
@@ -0,0 +1,20 @@
+function Notebook(data)
+{
+	this.data = data;	
+}
+
+Notebook.prototype.render = function(panelID) {
+		
+};
+
+Notebook.prototype.getCell = function(index) {
+	
+};
+
+Notebook.prototype.getCells = function() {
+
+};
+
+Notebook.prototype.saveNotebook = function() {
+	
+};
