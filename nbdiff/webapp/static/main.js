adf58bed531e5a5bb6d672af2bcf6eab3e543693
bsplay <bsplay@gmail.com>
2014-01-25 17:50:24 -0500

    drag and drop cells UI

--- nbdiff/webapp/static/main.js
+++ nbdiff/webapp/static/main.js
@@ -11,17 +11,48 @@ $( document ).ready(function() {
 				var baseCells = base.data.worksheets[0].cells.length;
 				var localCells = local.data.worksheets[0].cells.length;
 				var remoteCells = remote.data.worksheets[0].cells.length;
-				for(var i = 0; i < baseCells; i++)
-				{
-					var $row = $("<tr id="+i+"></tr>").appendTo("#merge_table > tbody")
-					$("<td class='local'></td>").appendTo($row);
-					$("<td class='base'></td>").appendTo($row);
-					$("<td class='remote'></td>").appendTo($row);
-				}
+				createTable(baseCells);
 				local.render("local");
 				base.render("base");
 				remote.render("remote");
 				} 
 		)
 	});
+	
+	function createTable(rows) {
+		$("#merge_table > tbody").empty();
+		$("#merge_table").append("<tr><td><p>Left Side Content:</p></td><td><p>Results:</p></td><td><p>Right Side Content:</p></td></tr>")
+		for(var i = 0; i < rows; i++)
+		{
+			var $row = $("<tr id="+i+"></tr>").appendTo("#merge_table > tbody")
+			$("<td class='local' draggable='true' ondragenter='dragEnter(event)' ondragstart='drag(event)' ondrop='drop(event)' ondragover='allowDrop(event)'></td>").appendTo($row);
+			$("<td class='base' draggable='true' ondragenter='dragEnter(event)' ondragstart='drag(event)' ondrop='drop(event)' ondragover='allowDrop(event)'></td>").appendTo($row);
+			$("<td class='remote' draggable='true' ondragenter='dragEnter(event)' ondragstart='drag(event)' ondrop='drop(event)' ondragover='allowDrop(event)'></td>").appendTo($row);
+		}
+	}
+	
 });
+
+function dragEnter(ev) {
+   ev.preventDefault();
+   return true;
+}
+
+function allowDrop(ev)
+{
+	ev.preventDefault();
+}
+
+function drag(ev)
+{
+	var data = ev.target.parentNode.id +" > td."+ ev.target.className;
+	ev.dataTransfer.setData("data", data);
+}
+
+function drop(ev)
+{
+	ev.preventDefault();
+	var data=ev.dataTransfer.getData("data");
+	var $target = $(ev.target);
+	$target.closest("td").html($('#'+data).html())
+}
