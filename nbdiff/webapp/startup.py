234af7e511f58502a31dfffde874fa35008283af
bsplay <bsplay@github.com>
2014-02-05 18:23:04 -0500

    renamed some files

--- nbdiff/webapp/startup.py
+++ nbdiff/webapp/startup.py
@@ -12,7 +12,7 @@ def merge():
     #do magical comparison here /*
     comparedData = open("example-premerged-notebook.ipynb").read()
     
-    return render_template('Diff.html', comparedData = comparedData)
+    return render_template('Merge.html', comparedData = comparedData)
     
 
 if __name__ == "__main__":
