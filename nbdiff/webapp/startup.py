0658dee56568ea12ea280d4e60d4a5f93c7d70c9
NightlyBuilds <richard_2001_year@hotmail.com>
2014-02-05 18:55:07 -0500

    Added css, header, footer, parent page inheritance

--- nbdiff/webapp/startup.py
+++ nbdiff/webapp/startup.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template, request
+import urllib2
+
 app = Flask('NB Diff')
 
 @app.route("/")
@@ -13,7 +15,22 @@ def merge():
     comparedData = open("example-premerged-notebook.ipynb").read()
     
     return render_template('Diff.html', comparedData = comparedData)
+
+@app.route("/mergeURL", methods=['GET', 'POST'])
+def mergeURL():
+    #https://www.dropbox.com/s/08n4aq6u6630iv1/left.ipynb
+    #https://dl.dropboxusercontent.com/s/08n4aq6u6630iv1/left.ipynb?dl=1&token_hash=AAGIQYzvIgsF1xvONTAtlhQK-kXPEcEIVgAgWLRnjAXInw
+    localURL = request.form['localURL']
+    localFile = urllib2.urlopen(localURL)
+    localJSON = localFile.read()
     
+    remoteURL = request.form['remoteURL']
+    remoteFile = urllib2.urlopen(remoteURL)
+    remoteJSON = remoteFile.read()
+    
+    #do magical comparison here /*
+    comparedData = open("example-premerged-notebook.ipynb").read()
+    return render_template('Diff.html', comparedData = comparedData)
 
 if __name__ == "__main__":
     app.debug = True
